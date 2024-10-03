90945bc049b1125176880065fc1f0726a6bdc2a9
ys <241280843@qq.com>
2024-10-04 02:00:59 +0800

    initial

---
+++ src/types.ts
@@ -0,0 +1,8 @@
+let num: number = 2;
+let str: string = '';
+let bool: boolean = true;
+let bigNum: bigint = BigInt( 1 );
+let sym: symbol = Symbol( 'union' );
+let empty: null = null;
+let unde: undefined = undefined;
+
